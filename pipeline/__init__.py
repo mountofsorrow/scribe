7ed74d71cacb19c194f8609232aeb5b56c96f404
mahdi masoumi <135983839+mountofsorrow@users.noreply.github.com>
2026-06-10 23:30:56 +0330

    Update __init__.py

---
+++ pipeline/__init__.py
@@ -0,0 +1,4 @@
+from pipeline.download import download_lecture
+from pipeline.transcribe_audio import load_model, transcribe
+from pipeline.summarize_file import run_pipeline
+from pipeline.query import query_single_session, query_entire_course
